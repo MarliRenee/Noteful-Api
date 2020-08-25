022f32b0b44d3df459ecc9cd779d43cdcf0fd7a2
Marli Renee <marlirenee@gmail.com>
2020-08-25 15:27:16 -0700

    edits to postgrator-config.js for ssl certification

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,6 +4,7 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
   //Ternary condition that checks the NODE_ENV and either uses DATABASE_URL or TEST_DATABASE_URL
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
